8db0ef834373fdae47a7b65fc9fae2a6c0b3515e
jennifermel <jennifermelelissa8503@gmail.com>
2024-09-20 15:34:09 -0300

    usando um unico objeto para fazer tudo(melhor forma de ser feito)

--- app.py
+++ app.py
@@ -11,16 +11,19 @@ if __name__=='__main__':
     endereco_pessoal = Endereco('','','','')
     usuario = Pessoa('',0,'')
 
-    #entrada de dados 
+    #entrada de dados usuario
     usuario.nome            = input('Informe o nome do usuario: ')
     usuario.idade           = int(input('Informe a idade do usuario: '))
+
+    #composicao usuario-endereco
+    usuario.endereco        = endereco_pessoal
+
+    #entrada de dados endereco
     endereco_pessoal.cep    = input('Informe o CPF: ')
     endereco_pessoal.uf     = input('Informe a UF: ')
     endereco_pessoal.cidade = input('Informe a cidade: ')
     endereco_pessoal.bairro = input('Informe o bairro: ')
 
-    #composicao usuario-endereco
-    usuario.endereco        = endereco_pessoal
 
     #saida de dados
     print(usuario.obter_info_pessoal())
